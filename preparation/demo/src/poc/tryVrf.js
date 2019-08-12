9aa8c4d5008d5f32a9f5600ece3304a293e31cfb
kevingzhang <kevin.zhang.canada@gmail.com>
2019-08-11 22:27:22 -0700

    VRF sortition test

--- preparation/demo/src/poc/tryVrf.js
+++ preparation/demo/src/poc/tryVrf.js
@@ -18,31 +18,44 @@ exports.tryVrf = (req, res)=>{
   // console.log('result,', result);
 
 
-  const times = parseInt(req.query.times || '5');
-  const w = new Big(parseInt(req.query.w || '10'));
-  const W = new Big(parseInt(req.query.W || '1000'));
+  const times = parseInt(req.query.times || '10');
+  const w = new Big(parseInt(req.query.w || '10'));  // 持币量
+  const W = new Big(parseInt(req.query.W || '1000'));  // 总币量
   const tau = new Big(parseInt(req.query.tau || '10'));
 
+  const getAverangeResult = (f, times) =>{
+    let sum = 0;
+    for (let i = 0; i < times; i++) {
+      sum += f();
+    }
+    return (sum / times).toFixed(2);
+  };
   
-  const seed = Buffer.from('previousBlockHashUnpredictable')
-  const role = Buffer.from('luckyDraw')
-
-  let success = 0;
-  let fail = 0;
-  for (let i = 0; i < times; i++) {
-    const [publicKey, privateKey] = utils.generatePair()
-    const [value, proof, j] = sortition.sortition(privateKey, publicKey, seed, tau, role, w, W)
-    log(`-------------- test ${i} --------------`)
-    log(' value:', value.toString('hex'))
-    log('     j:', j.toString())
-    log('result:', j.gt(0))
-    const result = j.gt(0);
-    success += result? 1:0;
-    
-    fail += result? 0: 1;
+  console.log('C(5,0)', sortition.combination(new Big(5), new Big(0)).toString());
+  
+  // 希望多少人被选出来
+  for (let a = 1; a <= times; a++) {
+    const p = a / W;
+  
+    const c = getAverangeResult(() => {
+      let count = 0;
+      for (let i = 0; i < 10; i++) {
+  
+        const [pub, pri] = utils.generatePair();
+  
+        const { proof, value } = ecvrf.vrf(pub, pri, utils.B([12]));
+  
+        const j = sortition.getVotes(value, new Big(w), new Big(p));
+        j.gt(0) && count++;
+  
+        // easierCheckVote(vrf, new Big(w), new Big(p)) && count++
+      }
+      return count;
+    }, 10);
+  
+    console.log('   count: ', a, '\t->', c);
   }
-  log("Success:", success);
-  log("Fail", fail);
+  
 
   
   res.send("vrf test");
