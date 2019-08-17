9953251fa26eaa43090db5fbdd9e6f99f145f835
kevingzhang <kevin.zhang.canada@gmail.com>
2019-08-17 12:08:51 -0700

    ask server to get user input a room postfix number everytime when start. We used a random member before, but I feel it is better to keep a number so that we do not have to reopen browser simulator everytime when server restart. beter for debug and test

--- preparation/demo/src/index.js
+++ preparation/demo/src/index.js
@@ -10,51 +10,59 @@ const {ipfsStart} = require('./poc/layerOneBlock/ipfsMod');
 const {channelListener} = require('./poc/layerOneBlock/channelListener');
 const {generateBlock} = require('./poc/layerOneBlock/generateBlock');
 const {utils} = require('vrf.js');
-//const blockService = require('./service/BlockServices');
-// open mongoose connection
-//mongoose.connect();
 
+// Get process.stdin as the standard input object.
+var standard_input = process.stdin;
 
+// Set input character encoding.
+standard_input.setEncoding('utf-8');
 
+// Prompt user to input data in console.
+console.log("Please input a random pubsub room postfix. Press enter to get a random number as default:");
 
-ipfsStart()
-.then((ipfs)=>{
-  app.set('ipfs', ipfs);
-  const randRoomPostfix = Math.round(Math.random()*1000).toString();//Every time we start the server, we create a new rand string so that the pub sub channel would be different each time. This will help debuging since other users may also use the same code base to test and publish message to the same room
-  app.set('randRoomPostfix', randRoomPostfix);
-  console.log("Generating 100 preset users, please wait a few seconds...");
-  let presetUsers = [];
-  for(let i = 0; i < 100; i ++){
-    const [publicKey, privateKey] = utils.generatePair();
-    const u ={
-      name:'user #' + i,
-      pub:publicKey.toString('hex'),
-      pri:privateKey.toString('hex')
+// When user input data and click enter key.
+standard_input.on('data', function (data) {
+  const randRoomPostfix = data? parseInt(data) : Math.round(Math.random()*1000).toString();
+  ipfsStart()
+  .then((ipfs)=>{
+    app.set('ipfs', ipfs);
+    app.set('randRoomPostfix', randRoomPostfix);
+    console.log("Generating 100 preset users, please wait a few seconds...");
+    let presetUsers = [];
+    for(let i = 0; i < 20; i ++){
+      const [publicKey, privateKey] = utils.generatePair();
+      const u ={
+        name:'user #' + i,
+        pub:publicKey.toString('hex'),
+        pri:privateKey.toString('hex')
+      }
+      presetUsers.push(u);
     }
-    presetUsers.push(u);
-  }
-  console.log("presetUsers,", presetUsers);
-  app.set('presetUsers', presetUsers);
-  return channelListener(app.get('ipfs'), randRoomPostfix, presetUsers);
-})
-.then(({ipfs, globalState, pubsubRooms})=>{
-  app.set('pubsubRooms', pubsubRooms);
-  app.set('globalState', globalState);
-  const {blockRoom} = pubsubRooms;
-  const blockGenerationInterval = 1000*30;
-  const firstBlockDelay = 1000 * 3;
+    console.log("presetUsers,", presetUsers);
+    app.set('presetUsers', presetUsers);
+    return channelListener(app.get('ipfs'), randRoomPostfix, presetUsers);
+  })
+  .then(({ipfs, globalState, pubsubRooms})=>{
+    app.set('pubsubRooms', pubsubRooms);
+    app.set('globalState', globalState);
+    const {blockRoom} = pubsubRooms;
+    const blockGenerationInterval = 1000*30;
+    const firstBlockDelay = 1000 * 3;
 
-  // const loop = async ({ipfs, globalState, blockRoom})=>{
-  //   await generateBlock({ipfs, globalState, blockRoom});
-  //   _.delay(loop, blockGenerationInterval, {ipfs, globalState, blockRoom});
-  // };
-  //_.delay(loop, firstBlockDelay, {ipfs, globalState, blockRoom});
-  _.delay(generateBlock, firstBlockDelay, {ipfs, globalState, blockRoom});
-  //console.log("in index.js init, pubsubRooms in app:", pubsubRooms);
-})
+    // const loop = async ({ipfs, globalState, blockRoom})=>{
+    //   await generateBlock({ipfs, globalState, blockRoom});
+    //   _.delay(loop, blockGenerationInterval, {ipfs, globalState, blockRoom});
+    // };
+    //_.delay(loop, firstBlockDelay, {ipfs, globalState, blockRoom});
+    _.delay(generateBlock, firstBlockDelay, {ipfs, globalState, blockRoom});
+    //console.log("in index.js init, pubsubRooms in app:", pubsubRooms);
+  })
 
-// listen to requests
-app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+  // listen to requests
+  app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+
+});
+
 
 /**
 * Exports express
